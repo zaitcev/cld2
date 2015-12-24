9b82e94ca650bf7ce549ced61118246ae6e97672
Pete Zaitcev <zaitcev@kotori.zaitcev.us>
2015-12-24 01:36:14 -0700

    WIP 1
    main()
    os.Args through flag

---
+++ cld2.go
@@ -0,0 +1,34 @@
+// CLD2
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	// "net"
+	"os"
+)
+
+const TAG = "cld2"
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage: cld2 [-h host] [-p port]\n")
+}
+
+func main() {
+	// if len(os.Args) > 1 {
+	// 	fmt.Fprintf(os.Stderr, "Usage: cld2\n");
+	// 	os.Exit(1)
+	// }
+
+	var listenhost string
+	var listenport int
+
+	flag.StringVar(&listenhost, "h", "", "Hostname to bind for listening")
+	flag.IntVar(&listenport, "p", 8081, "Port to bind for listening")
+
+	flag.Usage = usage
+	flag.Parse()
+
+	fmt.Printf("host %s port %d\n", listenhost, listenport) // P3
+}
